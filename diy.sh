d41764a2b24a4a0a4019a096ad085dbbe45a6ff0
SuperManito <8032484+SuperManito@user.noreply.gitee.com>
2021-04-04 02:02:16 +0800

    update

--- diy.sh
+++ diy.sh
@@ -11,6 +11,8 @@ author_list="SuperManito"
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
 
+scripts_base_url_1=https://gitee.com/SuperManito/scripts/raw/master/
+
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.sevencdn.com/whyour/hundun/master/quanx/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
